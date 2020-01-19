64285e648ccf0835690e91d81f064785e9dcc137
Karn Dalmia <karndalmia@gmail.com>
2020-01-18 19:42:34 -0800

    Update playingWithNumbers.py

--- playingWithNumbers.py
+++ playingWithNumbers.py
@@ -1,18 +1,28 @@
 from sys import stdin
-
-def findLargest(strNum): #input strNum, output largest possible int
+from itertools import permutations
+def factorial(n):
+    if(n<0): return Exception
+    elif(n==0 or n==1): return 1
+    else: return reduce(lambda x,y: x*y, [i for i in range(2,n)])
 	
-			
+def findPowerList(str_num_L,saveCount):
+    len_l = len(str_num_L)
+    n_combos = factorial(len_l)/(factorial(saveCount)*factorial(len_l-saveCount))	
+    perm = list(set(filter(lambda perm: len(perm)==saveCount, list(permutations(str_num_L)))))
+    return list(map(lambda x: int("".join(x)), perm ))	     
 
-def findSmallest(strNum): #input strNum, output largest possible int
-	
+def findLargestSmallest(strNum, deleteCount): #input strNum, output largest possible int
+    saveCount=None,None,len(strNum-deleteCount)
+    strNum_l=strNum.split("")
+    powerList = sorted(findPowerList(str_num_L,saveCount)) 
+    return powerList[0],powerList[-1]
 	
 
 def findWrapper(strNum):
-	deleteCount = int(strNum[-1])
-	strNum = strNum[:len(strNum) - 2]
-	print(findSmallest(strNum, deleteCount))
-	print(findLargest(strNum, deleteCount))
+	strNumP_l = strNum.split(" ")
+	strNum, deleteCount = strNumP_l[0], strNumP_l[1]
+	print(findLargestSmallest(strNum, deleteCount))
+	
 
 
 test = """3
@@ -21,8 +31,8 @@ test = """3
 234714812741111111111111111111 4"""
 sdi = (stdin.readlines())#list of strings
 sdi = test.split("\n")
-sdi = sdi[1:] #who cares of that first element
+sdi = sdi[1:] #who cares of that first element(length of list python native)
 for elem in sdi:
-	elem = elem.rstrip()
+	elem = elem.strip()
 	print(elem)
 	print(findWrapper(elem))
